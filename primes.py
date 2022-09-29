f24785cb8cde93cad49d23a7c6f46a32c5748ab6
Baldeep Gill <baldeep.gill@kcl.ac.uk>
2022-09-29 16:04:26 +0100

    Complete first attempt at exercise Primes

--- primes.py
+++ primes.py
@@ -3,4 +3,13 @@
 
 def primes(number_of_primes):
     list = []
+    number = 2
+    while len(list) < number_of_primes:
+        prime = True
+        for i in range(2, number):
+            if (number % i == 0):
+                prime = False
+        if prime:
+            list.append(number)
+        number = number + 1
     return list
